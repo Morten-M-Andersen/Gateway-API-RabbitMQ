21b278657e5e9f76a4b9d87faf7d83611dfa0ed2
Morten Miland Andersen <10407527@ucn.dk>
2025-01-08 20:36:20 +0100

    Added "Bogus" NuGet package for generating test data.

--- Gateway/Controllers/AddBookController.cs
+++ Gateway/Controllers/AddBookController.cs
@@ -3,6 +3,7 @@ using RabbitMQ.Client;
 using Serilog;
 using System.Text.Json;
 using System.Text;
+using Bogus;
 using Gateway.Messages;
 using Gateway.Models;
 
@@ -49,6 +50,44 @@ namespace Gateway.Controllers
             catch(Exception ex)
             {
                 Log.Error(ex, "An error occured while processing the AddBook request.");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        // POST api/<AddBookController>/GenerateBooks
+        [HttpPost("GenerateBooks")]
+        public async Task<IActionResult> GenerateBooks([FromQuery] int count, [FromQuery] int seed)
+        {
+            try
+            {
+                if (count <= 0)
+                {
+                    return BadRequest("Count must be greater than 0.");
+                }
+
+                // Configure Bogus with seed - use same seed to generate same data again
+                Randomizer.Seed = new Random(seed);
+
+                // Create a Faker for AddBook
+                var bookFaker = new Faker<AddBook>()
+                    .RuleFor(b => b.ISBN, f => f.Commerce.Ean13()) // Generate random ISBNs
+                    .RuleFor(b => b.Title, f => f.Lorem.Sentence(3)) // Generate random book titles
+                    .RuleFor(b => b.Author, f => f.Name.FullName()); // Generate random author names
+
+                // Generate the specified number of AddBook objects
+                var books = bookFaker.Generate(count);
+
+                // Send each book message
+                foreach (var book in books)
+                {
+                    await _messageProducer.SendMessage(book);
+                }
+
+                return Ok($"{count} books generated and sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred while generating books.");
                 return StatusCode(500, "Internal server error");
             }
         }
